5cd96658c6af192f70579aa5dbe57d61311ef050
Nadav Abuhatsira <Nudu22@gmail.com>
2019-06-05 15:57:54 +0300

    Added DrawOnMouseMove + Changed Triangle to responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,96 @@
-function init(){
-    console.log('CONNECTED');
+'use strict'
+
+let canvas
+let ctx
+
+let currElement = 'triangle'
+
+function changeEl(elName) {
+    currElement = elName
 }
+
+function init() {
+    canvas = document.querySelector('#my-canvas');
+    ctx = canvas.getContext('2d')
+    canvas.width = window.innerWidth - 50
+    canvas.height = window.innerHeight - 100
+}
+
+function draw(ev) {
+    ctx.save()
+    const { offsetX, offsetY } = ev
+    switch (currElement) {
+        case 'triangle':
+            drawTriangle(offsetX, offsetY)
+            break;
+        case 'rect':
+            drawRect(offsetX, offsetY)
+            break;
+        case 'text':
+            drawText('test', offsetX, offsetY)
+            break;
+    }
+    ctx.restore()
+}
+
+function downloadCanvas(elLink) {
+    const data = canvas.toDataURL()
+    elLink.href = data
+
+
+
+    elLink.download = 'my-img.jpg'
+}
+
+function drawImg() {
+    const img = document.querySelector('img');
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+
+}
+
+function clearCanvas() {
+    ctx.fillStyle = 'yellow'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(50, 50, 100, 100)
+}
+
+function drawText(txt, x, y) {
+    ctx.fillStyle = 'white'
+    ctx.strokeStyle = 'green'
+    ctx.font = "17px Arial";
+    ctx.strokeText(txt, x, y);
+}
+
+
+function drawArc() {
+    ctx.beginPath();
+    ctx.arc(100, 75, 50, 0, 1 * Math.PI);
+    ctx.stroke();
+}
+
+function drawRect(x, y) {
+    ctx.rect(x, y, 150, 150)
+    ctx.fillStyle = 'orange'
+    ctx.fillRect(x, y, 150, 150)
+    ctx.stroke()
+    ctx.fill()
+}
+
+
+
+function drawTriangle(x,y) {
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+    ctx.lineTo(x + 300,y + 150);
+    ctx.lineTo(x + 100, y +  100);
+    ctx.closePath()
+
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = 'blue'
+    ctx.fillStyle = '#ff0000'
+
+    ctx.stroke();
+    ctx.fill()
+
+}
+
